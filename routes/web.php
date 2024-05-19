fb8f90de6a93a9725d0f7cd526d0862dcb13b19d
brndalx <113251192+brandalx@users.noreply.github.com>
2024-05-18 22:07:34 -0500

    feat: abut us route, homepage repsr

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "<h1> Homepage </h1> <a href='/about'> About Us </a>"; 
+});
+
+//about us route
+Route::get('/about', function (){
+    return  "<h1> About Page </h1> <a href='/'> Back to home page </a>"; 
 });
