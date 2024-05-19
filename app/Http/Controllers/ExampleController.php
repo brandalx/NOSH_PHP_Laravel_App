ac4785fed03e805226eee40cb5bb3378471a9db1
brndalx <113251192+brandalx@users.noreply.github.com>
2024-05-18 22:29:49 -0500

    feat: example controller inner funcs

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class ExampleController extends Controller
 {
-    //
+    public function homePage() {
+     return   "<h1> Homepage </h1> <a href='/about'> About Us </a>"; 
+    }
+
+
+    public function aboutPage() {
+        return     "<h1> Homepage </h1> <a href='/about'> About Us </a>"; 
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExampleController;
+// old way to use
+// Route::get('/', function () {
+//     return "<h1> Homepage </h1> <a href='/about'> About Us </a>"; 
+// });
 
-Route::get('/', function () {
-    return "<h1> Homepage </h1> <a href='/about'> About Us </a>"; 
-});
+// //about us route, example of adding controller
+// Route::get('/about', function (){
+//     return  "<h1> About Page </h1> <a href='/'> Back to home page </a>"; 
+// });
 
-//about us route, example of adding controller
-Route::get('/about', function (){
-    return  "<h1> About Page </h1> <a href='/'> Back to home page </a>"; 
-});
+
+
+Route::get('/', [ExampleController::class, "homePage"]);
+
+Route::get('/about', [ExampleController::class, "aboutPage"]);
